410a9dacd5c7c41132715c230d616b7b083fe234
Phornee <phornee@gmail.com>
2020-12-27 11:52:02 +0100

    fix name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="DBConn",
+    name="ManagedClass",
     version="0.0.2",
     author="Ismael Raya",
     author_email="phornee@gmail.com",
