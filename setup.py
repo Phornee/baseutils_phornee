dcb167f859d11d0f7f87b98b298494df7ef8018b
Phornee <phornee@gmail.com>
2020-12-29 08:24:31 +0100

    rename

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ManagedClass",
+    name="baseutils",
     version="0.0.2",
     author="Ismael Raya",
     author_email="phornee@gmail.com",
-    description="Base Managed Class",
+    description="Utility modules for Class management",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Phornee/ManagedClass",
+    url="https://github.com/Phornee/baseutils",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
