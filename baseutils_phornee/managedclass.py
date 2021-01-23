1a9cf5ebb6f1ae436bcfc6d66165216f6d278266
Phornee <phornee@gmail.com>
2021-01-23 10:40:05 +0100

    created files in var

--- baseutils_phornee/managedclass.py
+++ baseutils_phornee/managedclass.py
@@ -9,18 +9,23 @@ from shutil import copyfile
 class ManagedClass:
 
     def __init__(self, classname, execpath):
-        self.execfolder = Path(execpath).parent
+        self._installfolder = Path(execpath).parent
+        self.homevar = "{}/var/{}".format(str(Path.home()), classname)
+
+        if not os.path.exists(self.homevar):
+            os.makedirs(self.homevar)
+
         self.readConfig()
         self.setupLogger(classname)
 
     def setupLogger(self, classname,):
         self.logger = logging.getLogger('{}_log'.format(classname))
-        log_folder = os.path.join(self.execfolder, self.config['logpath'])
+        log_folder = os.path.join(self.homevar, self.config['logpath'])
 
         if not os.path.exists(log_folder):
             os.mkdir(log_folder)
 
-        log_path = os.path.join(self.execfolder, self.config['logpath'], "{}.log".format(classname))
+        log_path = os.path.join(self.homevar, self.config['logpath'], "{}.log".format(classname))
 
         self.logger.setLevel(logging.INFO)
         fh = RotatingFileHandler(log_path, maxBytes=10000, backupCount=10)
@@ -29,12 +34,13 @@ class ManagedClass:
         self.logger.addHandler(fh)
 
     def readConfig(self):
-        config_yml_path = os.path.join(self.execfolder, 'config.yml')
+        config_yml_path = os.path.join(self.homevar, 'config.yml')
 
         # If config file doesn't exist yet, create it from the template
         if not os.path.isfile(config_yml_path):
-            config_template_yml_path = os.path.join(self.execfolder, 'config-template.yml')
+            config_template_yml_path = os.path.join(self._installfolder, 'config-template.yml')
             copyfile(config_template_yml_path, config_yml_path)
 
         with open(config_yml_path, 'r') as config_file:
             self.config = yaml.load(config_file, Loader=yaml.FullLoader)
+
