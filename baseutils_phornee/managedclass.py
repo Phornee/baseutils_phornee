0e2ae0c2be54b8aa2bfd6a174b24bfe64b693f4c
Phornee <phornee@gmail.com>
2021-04-24 23:24:04 +0200

    Raspberry Pi check function

--- baseutils_phornee/managedclass.py
+++ baseutils_phornee/managedclass.py
@@ -65,3 +65,46 @@ class ManagedClass(ABC):
 
         with open(config_yml_path, 'w') as config_file:
             yaml.dump(config, config_file)
+
+
+    def is_raspberry_pi(self, raise_on_errors=False):
+        """Checks if Raspberry PI.
+           :return:
+        """
+        import io
+        try:
+            with io.open('/proc/cpuinfo', 'r') as cpuinfo:
+                found = False
+                for line in cpuinfo:
+                    if line.startswith('Hardware'):
+                        found = True
+                        label, value = line.strip().split(':', 1)
+                        value = value.strip()
+                        if value not in (
+                            'BCM2708',
+                            'BCM2709',
+                            'BCM2711',
+                            'BCM2835',
+                            'BCM2836'
+                        ):
+                            if raise_on_errors:
+                                raise ValueError(
+                                    'This system does not appear to be a '
+                                    'Raspberry Pi.'
+                                )
+                            else:
+                                return False
+                if not found:
+                    if raise_on_errors:
+                        raise ValueError(
+                            'Unable to determine if this system is a Raspberry Pi.'
+                        )
+                    else:
+                        return False
+        except IOError:
+            if raise_on_errors:
+                raise ValueError('Unable to open `/proc/cpuinfo`.')
+            else:
+                return False
+
+        return True
